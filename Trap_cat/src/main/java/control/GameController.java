68b82e09b0be180951533ed25df174026bd8bff1
Santiago Arboleda Velasco <72291029+Santi1761@users.noreply.github.com>
2023-11-24 19:18:01 -0500

    Update Assets

--- Trap_cat/src/main/java/control/GameController.java
+++ Trap_cat/src/main/java/control/GameController.java
@@ -2,8 +2,10 @@ package control;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 public class GameController {
     @FXML
@@ -17,6 +19,20 @@ public class GameController {
 
     @FXML
     private void startGame() {
-        // Add your logic here
+        drawCat();
+    }
+
+    private void drawCat() {
+        GraphicsContext gc = gameCanvas.getGraphicsContext2D();
+
+        // Cargar la imagen del gato
+        Image catImage = new Image("/assets/Logo.png");
+
+        // Dibujar la imagen del gato en el centro del Canvas
+        double x = (gameCanvas.getWidth() - catImage.getWidth()) / 2;
+        double y = (gameCanvas.getHeight() - catImage.getHeight()) / 2;
+
+        gc.clearRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight()); // Limpiar el Canvas antes de dibujar
+        gc.drawImage(catImage, x, y);
     }
 }
