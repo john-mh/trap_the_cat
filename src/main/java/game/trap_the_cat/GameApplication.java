9f629486a6bc85e88483c9e43e80742b5c5dd782
John Hortua <1109660285@u.icesi.edu.co>
2023-11-25 09:56:08 -0500

    New window methods

--- src/main/java/game/trap_the_cat/GameApplication.java
+++ src/main/java/game/trap_the_cat/GameApplication.java
@@ -9,16 +9,31 @@ import java.io.IOException;
 
 public class GameApplication extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/game/trap_the_cat/game.fxml"));
-        Scene scene = new Scene(loader.load(), 600, 600);
-        stage.setTitle("TRAP THE CAT");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        showWindow("start", stage, 600, 500);
+    }
+
+    public static void hideWindow(Stage window) {
+        window.hide();
+    }
+
+    public static void showWindow(String fxml, Stage stage, double width, double height){
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource(fxml+".fxml"));
+        Scene scene;
+        try {
+            scene = new Scene(fxmlLoader.load(), width, height);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(stage == null) stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Trap the Cat!");
+        stage.setResizable(false);
+        stage.show();
     }
 }
