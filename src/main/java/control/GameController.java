d87d2696f6a5e281261bd8011f172c08c4721779
John Hortua <1109660285@u.icesi.edu.co>
2023-11-25 09:56:32 -0500

    Now the image shows

--- src/main/java/control/GameController.java
+++ src/main/java/control/GameController.java
@@ -2,8 +2,12 @@ package control;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -14,13 +18,13 @@ import javafx.scene.shape.Polygon;
 import javafx.util.Duration;
 import model.logic.GameManager;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.Timer;
 
-public class GameController {
-    public GameController() {
-    }
+public class GameController implements Initializable {
 
     static final Paint BLOCKED = Color.rgb(84, 99, 17);
     static final Paint OPEN = Color.rgb(218, 247, 12);
@@ -33,19 +37,27 @@ public class GameController {
 
     @FXML
     private ImageView catLeft;
+    private boolean canClick = true;
 
-    //TODO transition from bilaiase to closed
+    //TODO ckeck transition
     @FXML
     private void onMouseClicked(MouseEvent event) {
-
         Polygon polygon = (Polygon) event.getSource();
+
+        if (!canClick) return;
+
         if (polygon.getFill().equals(OPEN)) {
+            canClick = false;
             polygon.setFill(BLOCKED);
             int id = Integer.parseInt(polygon.getId().substring(4));
             //TODO tal vez mover esto a un metodo de GameManager
             GameManager.getInstance().getGraph().getVertex(id).setClosed(true);
             GameManager.getInstance().getGraph().deleteFromNeighbours(id);
             GameManager.getInstance().moveCat();
+
+            PauseTransition wait = new PauseTransition(Duration.seconds(1));
+            wait.setOnFinished(e -> canClick = true);
+            wait.play();
         } else {
             polygon.setFill(BILAIASE);
 
@@ -58,14 +70,20 @@ public class GameController {
         }
     }
 
-    @FXML
-    private void initialize() {
-        catLeft.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/game/trap_the_cat/cat_left.png"))));
-
-        for(int i = 0; i < floorPane.getChildren().size(); i++) {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (int i = 0; i < floorPane.getChildren().size(); i++) {
+            if(floorPane.getChildren().get(i) instanceof ImageView) continue;
             Polygon polygon = (Polygon) floorPane.getChildren().get(i);
             polygon.setFill(OPEN);
+            polygon.toBack();
             int id = Integer.parseInt(polygon.getId().substring(4));
-            GameManager.getInstance().getGraph().setPolygon(id + 1, polygon);
+            GameManager.getInstance().getGraph().setPolygon(id, polygon);
         }
+
+        this.catLeft.setImage(new Image("file:src/assets/GatoAbajo.png"));
+        this.catLeft.setLayoutX(Objects.requireNonNull(GameManager.getInstance().getGraph().getVertex(61)).getPolygon().getLayoutX());
+        this.catLeft.setLayoutY(Objects.requireNonNull(GameManager.getInstance().getGraph().getVertex(61)).getPolygon().getLayoutY());
+        this.catLeft.toFront();
+    }
 }
