5a1cdec6a297f946c6322f9ae0fec4d6e7691102
Santiago Arboleda Velasco <72291029+Santi1761@users.noreply.github.com>
2023-11-25 00:27:01 -0500

    Start mas o menos

--- Trap_cat/src/main/java/control/GameController.java
+++ Trap_cat/src/main/java/control/GameController.java
@@ -1,38 +1,36 @@
 package control;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class GameController {
-    @FXML
-    private Button startButton;
 
     @FXML
-    private Label scoreLabel;
+    private AnchorPane anchorPane;
 
     @FXML
-    private Canvas gameCanvas;
+    private ImageView logoImageView;
 
     @FXML
-    private void startGame() {
-        drawCat();
+    private TextField usernameTextField;
+
+    @FXML
+    private Button goButton;
+
+    @FXML
+    private void initialize() {
+       
     }
 
-    private void drawCat() {
-        GraphicsContext gc = gameCanvas.getGraphicsContext2D();
-
-        // Cargar la imagen del gato
-        Image catImage = new Image("/assets/Logo.png");
-
-        // Dibujar la imagen del gato en el centro del Canvas
-        double x = (gameCanvas.getWidth() - catImage.getWidth()) / 2;
-        double y = (gameCanvas.getHeight() - catImage.getHeight()) / 2;
-
-        gc.clearRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight()); // Limpiar el Canvas antes de dibujar
-        gc.drawImage(catImage, x, y);
+    @FXML
+    private void go() throws IOException {
+        // Aquí puedes agregar lógica para manejar el evento del botón "GO"
+        // Por ejemplo, cargar una nueva escena o realizar alguna acción.
     }
 }
