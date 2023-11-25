dd036c1108894e9ec45da8b4b4f133310f302b99
John Hortua <1109660285@u.icesi.edu.co>
2023-11-25 09:55:39 -0500

    Basic cat class

--- src/main/java/model/entity/Cat.java
+++ src/main/java/model/entity/Cat.java
@@ -1,4 +1,24 @@
 package model.entity;
 
+import javafx.scene.image.ImageView;
+
 public class Cat {
+
+    private static Cat cat;
+    private ImageView catImage;
+
+    private Cat(){}
+
+    public static Cat getInstance() {
+        if(cat == null) cat = new Cat();
+        return cat;
+    }
+
+    public ImageView getCatImage() {
+        return catImage;
+    }
+
+    public void setCatImage(ImageView catImage) {
+        this.catImage = catImage;
+    }
 }
