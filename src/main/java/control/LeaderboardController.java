5c7018fc4be8d2caab2f70c2b5cb0d37dafff273
John Hortua <1109660285@u.icesi.edu.co>
2023-11-25 11:02:32 -0500

    Added buttons

--- src/main/java/control/LeaderboardController.java
+++ src/main/java/control/LeaderboardController.java
@@ -1,69 +1,47 @@
 package control;
 
+import game.trap_the_cat.GameApplication;
+import model.entity.User;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
-import model.entity.User;
-import model.logic.GameManager;
 
 import java.io.IOException;
 
 import java.net.URL;
-import java.util.ArrayList;
+
 import java.util.ResourceBundle;
-
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-import javafx.scene.control.cell.PropertyValueFactory;
-
 
 public class LeaderboardController implements Initializable {
 
-    public LeaderboardController() {
-    }
-
-    GameManager manager;
-
-    public LeaderboardController(GameManager manager) {
-        this.manager = new GameManager();
-    }
-
     @FXML
     private TableColumn<User, Integer> posTc;
-
     @FXML
     private TableColumn<User, String> nameTc;
-
     @FXML
     private TableColumn<User, Integer> scoreTc;
-
     @FXML
     private Label msgLb;
-
     @FXML
     private TableView<User> tv;
-
-
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button newGameButton;
+    @FXML
+    private Button deleteScoreButton;
 
     @FXML
     public void startNewGame(ActionEvent event) throws IOException {
-
-        manager.exportData();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("start.fxml"));
-        loader.setController(this);
-        Parent parent = loader.load();
-        Stage st = new Stage();
-        st.setScene(new Scene(parent));
-        st.showAndWait();
+        //GameManager.getInstance().exportData();
+        GameApplication.hideWindow((Stage) exitButton.getScene().getWindow());
+        GameApplication.showWindow("start", null, 600, 500);
     }
 
     @FXML
@@ -73,15 +51,14 @@ public class LeaderboardController implements Initializable {
     }
 
     @FXML
-    public void exitGame(ActionEvent event){
-
+    public void exitGame(ActionEvent event) throws IOException {
+        //GameManager.getInstance().exportData();
+        GameApplication.hideWindow((Stage) exitButton.getScene().getWindow());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
-
 
     /**
     public void initializeLeaderBoard() {
